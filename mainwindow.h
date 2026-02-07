73e1a375e399729f39b87f2079ef4fb331f9d3d4
liuxinrui4787 <107227413+liuxinrui4787@users.noreply.github.com>
2026-02-07 10:19:11 +0800

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,71 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "basewindow.h"
+#include <QThread>
+#include "opencv2/core.hpp"
+#include "components/qtmaterialdialog.h"
+#include "view/qgraphicsviewifr.h"
+#include "view/stepview.h"
+#include "logpagetest.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public BaseWindow
+{
+    Q_OBJECT
+
+  public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    LogpageTest* log =new LogpageTest();
+
+  protected:
+    void init();
+
+  signals:
+    void currentShowPage(int);
+    void logStart();
+
+    void signalLog(int,QString);
+
+
+  private slots:
+    void setDebug(bool);
+    void onModuleSelect(int);
+    void on_stepDebug1_clicked();
+
+    void on_stepDebug2_clicked();
+
+    void on_stepDebug3_clicked();
+
+
+    void showWindowL(int viewId);
+    void showWindowR(int viewId);
+    void startProgressViewId(int id);
+    void completeViewId(int id);
+    void jumpViewId(int id);
+    void actualParaImg(QPixmap);
+    void dismapImg(QPixmap);
+    void slotSetLog(int,QString);
+
+    void on_btnExit_2_clicked();
+
+    void receiveDenFromcol(QString);
+    void receivedirectStrFrom(QString);
+    void receivePathFromConMain(QString);
+    void receiveYichangFromLog(QString);
+
+private:
+    Ui::MainWindow *ui;
+    QList<QGraphicsView*> GraphicsListL;
+    QList<QGraphicsView*> GraphicsListR;
+    QList<StepView *> stepList;
+
+};
+#endif // MAINWINDOW_H
